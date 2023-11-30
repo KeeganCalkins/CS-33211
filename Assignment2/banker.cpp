201ddc88dd287484f337b1f7c99e0fab11be1c30
Keegan Calkins <keegan2955@gmail.com>
2023-11-29 22:07:03 -0500

    modified function in implementation

--- Assignment2/banker.cpp
+++ Assignment2/banker.cpp
@@ -16,11 +16,11 @@ void getInput(std::vector<std::vector<int>> &allocation, std::vector<std::vector
     }
 
     char cIn; // holds the current character from file
-    int i;
+    int i = 0;
     std::string tempStr; 
     std::vector<int> vecRow; // holds the row as an int vector
 
-    while(in.eof()) {
+    while(!in.eof()) {
         in.get(cIn); // get the current character from the file
         if(!isdigit(cIn)) { // checks for when the current character is not an integer
             if(tempStr != "") {
@@ -33,21 +33,24 @@ void getInput(std::vector<std::vector<int>> &allocation, std::vector<std::vector
             tempStr = "";
         } else if(isdigit(cIn) && cIn != ' ' && cIn != ',') { // when a numeric value, push char onto string
             tempStr.push_back(cIn);
-        } else if(cIn == '\n') { // when end of row, clear current row before moving
+        } 
+        
+        if(cIn == '\n') { // when end of row, clear current row before moving
             i = 0;
             vecRow.clear();
         } else if(cIn == ',') { // check for comma
             if(i == 0) { // if the iterator is set to 0, push the row vector onto the allocation vector
                 allocation.push_back(vecRow);
-            }
-            if(i == 1) { // if the iterator is set to 1, push the row vector onto the max vector
+            } else if(i == 1) { // if the iterator is set to 1, push the row vector onto the max vector
                 max.push_back(vecRow);
             }
+            vecRow.clear(); // clear row values            
+            i++; // increment vector iterator
         }
     }
 
     processes = allocation.size(); // sets number of processes to allocation vector size
-    resources = max.size(); // sets number of resources to max vector size
+    resources = available.size(); // sets number of resources to max vector size
 }
 
 int main() {
@@ -57,6 +60,5 @@ int main() {
     std::vector<int> available;
 
     getInput(allocation, max, available, processes, resources);
-
 
 }
