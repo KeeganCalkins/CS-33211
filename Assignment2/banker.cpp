324cbce9620eb4d720a8a82db78753da8dc46efe
Keegan Calkins <keegan2955@gmail.com>
2023-11-29 22:58:44 -0500

    finished main banker implementation

--- Assignment2/banker.cpp
+++ Assignment2/banker.cpp
@@ -15,53 +15,53 @@ void getInput(std::vector<std::vector<int>> &allocation, std::vector<std::vector
         exit(1);
     }
 
-    char cIn; // holds the current character from file
+    char cIn; //holds the current character from file
     int i = 0;
     std::string tempStr; 
-    std::vector<int> vecRow; // holds the row as an int vector
+    std::vector<int> vecRow; //holds the row as an int vector
 
     while(!in.eof()) {
-        in.get(cIn); // get the current character from the file
-        if(!isdigit(cIn)) { // checks for when the current character is not an integer
+        in.get(cIn); //get the current character from the file
+        if(!isdigit(cIn)) { //checks for when the current character is not an integer
             if(tempStr != "") {
-                int tempNum = std::stoi(tempStr); // set val of tempNum to tempStr as an int
+                int tempNum = std::stoi(tempStr); //set val of tempNum to tempStr as an int
                 if(i == 2) {
-                    available.push_back(tempNum); // push tempNum value onto available vector
+                    available.push_back(tempNum); //push tempNum value onto available vector
                 }
                 vecRow.push_back(tempNum);
             }
             tempStr = "";
-        } else if(isdigit(cIn) && cIn != ' ' && cIn != ',') { // when a numeric value, push char onto string
+        } else if(isdigit(cIn) && cIn != ' ' && cIn != ',') { //when a numeric value, push char onto string
             tempStr.push_back(cIn);
         } 
         
-        if(cIn == '\n') { // when end of row, clear current row before moving
+        if(cIn == '\n') { //when end of row, clear current row before moving
             i = 0;
             vecRow.clear();
-        } else if(cIn == ',') { // check for comma
-            if(i == 0) { // if the iterator is set to 0, push the row vector onto the allocation vector
+        } else if(cIn == ',') { //check for comma
+            if(i == 0) { //if the iterator is set to 0, push the row vector onto the allocation vector
                 allocation.push_back(vecRow);
-            } else if(i == 1) { // if the iterator is set to 1, push the row vector onto the max vector
+            } else if(i == 1) { //if the iterator is set to 1, push the row vector onto the max vector
                 max.push_back(vecRow);
             }
-            vecRow.clear(); // clear row values            
-            i++; // increment vector iterator
+            vecRow.clear(); //clear row values            
+            i++; //increment vector iterator
         }
     }
 
-    processes = allocation.size(); // sets number of processes to allocation vector size
-    resources = available.size(); // sets number of resources to max vector size
+    processes = allocation.size(); //sets number of processes to allocation vector size
+    resources = available.size(); //sets number of resources to max vector size
 }
 
 int main() {
-    int processes, resources; // holds number of processes/resources 
+    int processes, resources; //holds number of processes/resources 
     std::vector< std::vector<int> > allocation;  
     std::vector< std::vector<int> > max;
     std::vector<int> available;
 
-    getInput(allocation, max, available, processes, resources); // invoke function gathering data from input file
+    getInput(allocation, max, available, processes, resources); //invoke function gathering data from input file
 
-    std::cout << "\nAllocation Resources\n"; // output Allocation table values
+    std::cout << "\nAllocation Resources\n"; //output Allocation table values
     for(int i = 0; i < processes; i++) {
         for(int j = 0; j < resources; j++) {
             std::cout << allocation[i][j] << " ";
@@ -69,7 +69,7 @@ int main() {
         std::cout << '\n';
     }
 
-    std::cout << "\nMax Resources\n"; // output Max table values
+    std::cout << "\nMax Resources\n"; //output Max table values
     for(int i = 0; i < processes; i++) {
         for(int j = 0; j < resources; j++) {
             std::cout << max[i][j] << " ";
@@ -77,29 +77,29 @@ int main() {
         std::cout << '\n';
     }
 
-    std::cout << "\nAvailable Resources\n"; // output available resource values
+    std::cout << "\nAvailable Resources\n"; //output available resource values
     for(int i = 0; i < resources; i++) {
         std::cout << available[i] << " ";
     }
     std::cout << "\n\n";
 
-    int finished[processes]; // vector holding finished processes
-    int ordered[processes]; // vector holding safe sequence order
-    int needed[processes][resources]; // vector holding needed resources
+    int finished[processes]; //vector holding finished processes
+    int ordered[processes]; //vector holding safe sequence order
+    int needed[processes][resources]; //vector holding needed resources
 
-    int proIt = 0; // process iterator
+    int proIt = 0; //process iterator
 
     for(int i = 0; i < processes; i++) {
         finished[i] = 0;
     }
 
-    for(int i = 0; i < processes; i++) { // find needed resource values
+    for(int i = 0; i < processes; i++) { //find needed resource values
         for(int j = 0; j < resources; j++) {
             needed[i][j] = max[i][j] - allocation[i][j];
         }
     }
 
-    std::cout << "Needed Resources\n"; // output needed resource values
+    std::cout << "Needed Resources\n"; //output needed resource values
     for(int i = 0; i < processes; i++) {
         for(int j = 0; j < resources; j++) {
             std::cout << needed[i][j] << " ";
@@ -108,29 +108,29 @@ int main() {
     }
     std::cout << "\n";
 
-    for(int i = 0; i < processes; i++) { // iterate through each process
-        for(int j = 0; j < processes; j++) { // find position in sequence through processes
+    for(int i = 0; i < processes; i++) { //iterate through each process
+        for(int j = 0; j < processes; j++) { //find position in sequence through processes
             if(finished[j] == 0) {
-                int flag = 0; // flag 0 as false when to define processes position in sequence
+                int flag = 0; //flag 0 as false when to define processes position in sequence
             
                 for(int k = 0; k < resources; k++) { 
-                    if(needed[j][k] > available[k]) { // sets flag to 1 or true when needed is greater
-                        flag = 1;                     // than the available value
+                    if(needed[j][k] > available[k]) { //sets flag to 1 or true when needed is greater
+                        flag = 1;                     //than the available value
                         break;
                     }
                 }
                 if (flag == 0) {
-                    ordered[proIt++] = j; // adds the processes position to the ordered sequence
+                    ordered[proIt++] = j; //adds the processes position to the ordered sequence
                     for(int l = 0; l < resources; l++) {
                         available[l] += allocation[j][l];
                     }
-                    finished[j] = 1; // finishes the specified process
+                    finished[j] = 1; //finishes the specified process
                 }
             }
         }
     }
     int flag = 1;
-    for(int i = 0; i < processes; i++) { // if not finished then the sequence is unsafe and flag set to 0
+    for(int i = 0; i < processes; i++) { //if not finished then the sequence is unsafe and flag set to 0
         if(finished[i] == 0) {
             flag = 0;
             std::cout << "Sequence is unsafe\n";
@@ -138,7 +138,7 @@ int main() {
         }
     }
 
-    if (flag == 1) { // if finished, then print the safe sequence in the correct order
+    if (flag == 1) { //if finished, then print the safe sequence in the correct order
         std::cout << "Safe sequence: \n";
         for(int i = 0; i < processes - 1; i++) {
             std::cout << " P" << ordered[i] << " to";
